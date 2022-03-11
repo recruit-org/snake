75442bea0ca0823e80824cf51be848d440e346fa
Mashry <kazi.mashry01@gmail.com>
2022-03-11 14:35:13 +0600

    done for module-1

--- pages/index.js
+++ pages/index.js
@@ -63,34 +63,61 @@ const getRandomCell = () => ({
   y: Math.floor(Math.random() * Config.width),
 });
 
-const Snake = () => {
+const customHook = () => {
   const getDefaultSnake = () => [
     { x: 8, y: 12 },
     { x: 7, y: 12 },
     { x: 6, y: 12 },
   ];
+
+  const getInitialFood = () => [{ x: 4, y: 10, counter: 0 }];
   const grid = useRef();
 
   // snake[0] is head and snake[snake.length - 1] is tail
   const [snake, setSnake] = useState(getDefaultSnake());
   const [direction, setDirection] = useState(Direction.Right);
 
-  const [food, setFood] = useState({ x: 4, y: 10 });
-  const [score, setScore] = useState(0);
+  const [foods, setFoods] = useState(getInitialFood());
+
+  const resetGame = () => {
+    setSnake(getDefaultSnake());
+    setDirection(Direction.Right);
+    setFoods(getInitialFood());
+  }
+
+  const addNewFood = () => {
+    let newFood = getRandomCell();
+    while (isSnake(newFood) && isFood(newFood)) {
+        newFood = getRandomCell();
+    }
+    newFood.counter = 0;
+
+    setFoods(prev => {
+      return [newFood, ...prev.filter(item => !isSnake(item))];
+    });
+  }
 
   // move the snake
   useEffect(() => {
     const runSingleStep = () => {
       setSnake((snake) => {
         const head = snake[0];
-        const newHead = { x: head.x + direction.x, y: head.y + direction.y };
+        const newHead = { 
+          x: (head.x + direction.x + Config.width) % Config.width, 
+          y: (head.y + direction.y + Config.height) % Config.height
+        };
 
         // make a new snake by extending head
         // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax
         const newSnake = [newHead, ...snake];
 
         // remove tail
+        if(!isFood(newHead))
         newSnake.pop();
+
+        if(isSnake(newHead)) {
+          resetGame();
+        }
 
         return newSnake;
       });
@@ -100,42 +127,39 @@ const Snake = () => {
     const timer = setInterval(runSingleStep, 500);
 
     return () => clearInterval(timer);
-  }, [direction, food]);
+  }, [direction, foods]);
 
   // update score whenever head touches a food
   useEffect(() => {
     const head = snake[0];
     if (isFood(head)) {
-      setScore((score) => {
-        return score + 1;
-      });
-
-      let newFood = getRandomCell();
-      while (isSnake(newFood)) {
-        newFood = getRandomCell();
-      }
-
-      setFood(newFood);
+      addNewFood();
     }
   }, [snake]);
 
   useEffect(() => {
+    const handleSetDirection = (opposite, curr) => {
+      setDirection(prev => {
+        if(prev !== opposite) return curr;
+        return prev;
+      });
+    }
     const handleNavigation = (event) => {
       switch (event.key) {
         case "ArrowUp":
-          setDirection(Direction.Top);
+          handleSetDirection(Direction.Bottom, Direction.Top);
           break;
 
         case "ArrowDown":
-          setDirection(Direction.Bottom);
+          handleSetDirection(Direction.Top, Direction.Bottom);
           break;
 
         case "ArrowLeft":
-          setDirection(Direction.Left);
+          handleSetDirection(Direction.Right, Direction.Left);
           break;
 
         case "ArrowRight":
-          setDirection(Direction.Right);
+          handleSetDirection(Direction.Left, Direction.Right);
           break;
       }
     };
@@ -144,13 +168,44 @@ const Snake = () => {
     return () => window.removeEventListener("keydown", handleNavigation);
   }, []);
 
+  // add food after every 3 sec
+  useEffect(() => {
+    const interval = setInterval(() => {
+      addNewFood();
+    }, 3000)
+
+    return () => clearInterval(interval);
+  }, []);
+
+  // remove food after 10 second it created
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setFoods(
+        foods => {
+          return foods.map(food => ({...food, counter: food.counter + 1})).filter(food => food.counter < 10);
+        }
+      );
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   // ?. is called optional chaining
   // see: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining
-  const isFood = ({ x, y }) => food?.x === x && food?.y === y;
+  const isFood = ({ x, y }) => foods.some(food => food.x === x && food.y === y);
 
   const isSnake = ({ x, y }) =>
     snake.find((position) => position.x === x && position.y === y);
 
+  return {
+    isFood, 
+    isSnake, 
+    score: snake.length - getDefaultSnake().length
+  }
+}
+
+const Snake = () => {
+  const { isFood, isSnake, score } = customHook();
   const cells = [];
   for (let x = 0; x < Config.width; x++) {
     for (let y = 0; y < Config.height; y++) {
